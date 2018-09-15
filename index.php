54ca299f39792f75c262738f42c0a6e692b550e3
TeraokaAkihiro <macrosfronter7@gmail.com>
2018-09-15 18:38:46 +0900

    init php file. add conection to DB.

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+  $url = parse_url(getenv('DATABASE_URL'));
+
+  $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+
+  $pdo = new PDO($dsn, $url['user'], $url['pass']);
+  var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
+
+?>
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+  <meta charset="UTF-8">
+  <title>Caffeine Values</title>
+</head>
+<body>
+  
+</body>
+</html>
