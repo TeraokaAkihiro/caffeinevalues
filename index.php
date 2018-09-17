0de4f50f24b6a92176901f714141d24957bbb06b
TeraokaAkihiro <macrosfronter7@gmail.com>
2018-09-17 18:33:53 +0900

    add DB connection config file.

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
 
-  $url = parse_url(getenv('DATABASE_URL'));
-
-  $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
-
-  $pdo = new PDO($dsn, $url['user'], $url['pass']);
+  require_once("./conf/conf.php");
+  
+  try{
+    $pdo = new PDO(DSN, DB_USER, DB_PASSWD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  }catch(PDOException $e){
+    echo 'Connection Error : ' . $e->getMessage() . '<br>';
+  }
   var_dump(phpversion());
   var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
-
 ?>
 <!DOCTYPE html>
 <html lang="jp">
