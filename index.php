0d482dce59c6a47d7d0154e1c66010cf0d24b88d
TeraokaAkihiro <macrosfronter7@gmail.com>
2018-09-17 21:59:08 +0900

    fix heroku db connection conf.

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
-
-  require_once("./conf/conf.php");
   
+  if(defined('DATABASE_URL')){
+    $heroku_postgresql_url = parse_url(getenv('DATABASE_URL'));
+    define('DB_USER', $heroku_postgresql_url['user']);
+    define('DB_PASSWD', $heroku_postgresql_url['pass']);
+    define('DB_NAME', substr($heroku_postgresql_url['path'], 1));
+    define('DSN', sprintf('pgsql:host=%s;dbname=%s', $heroku_postgresql_url['host'], DB_NAME));
+  }else{
+    // local database config
+    require_once("./conf/db_conf.php");
+  }
   try{
     $pdo = new PDO(DSN, DB_USER, DB_PASSWD);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
