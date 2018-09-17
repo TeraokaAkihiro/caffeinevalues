eedf944cc81351f44e53e62a7a8b1852ce7241c7
TeraokaAkihiro <macrosfronter7@gmail.com>
2018-09-17 22:33:20 +0900

    remove db connection code.

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <?php
   
-  if(defined('DATABASE_URL')){
-    echo 'defined DATABASE_URL';
-    //$heroku_postgresql_url = parse_url(getenv('DATABASE_URL'));
-    //define('DB_USER', $heroku_postgresql_url['user']);
-    //define('DB_PASSWD', $heroku_postgresql_url['pass']);
-    //define('DSN', sprintf('pgsql:host=%s;dbname=%s',
-    //  $heroku_postgresql_url['host'],
-    //  substr($heroku_postgresql_url['path'], 1)
-    //));
-  }else{
-    // local database config
-    require_once("./conf/db_conf.php");
-  }
-  try{
-    $pdo = new PDO(DSN, DB_USER, DB_PASSWD);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-  }catch(PDOException $e){
-    echo 'Connection Error : ' . $e->getMessage() . '<br>';
-  }
+  //if(defined('DATABASE_URL')){
+  //  echo 'defined DATABASE_URL';
+  //  //$heroku_postgresql_url = parse_url(getenv('DATABASE_URL'));
+  //  //define('DB_USER', $heroku_postgresql_url['user']);
+  //  //define('DB_PASSWD', $heroku_postgresql_url['pass']);
+  //  //define('DSN', sprintf('pgsql:host=%s;dbname=%s',
+  //  //  $heroku_postgresql_url['host'],
+  //  //  substr($heroku_postgresql_url['path'], 1)
+  //  //));
+  //}else{
+  //  // local database config
+  //  require_once("./conf/db_conf.php");
+  //}
+  //try{
+  //  $pdo = new PDO(DSN, DB_USER, DB_PASSWD);
+  //  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  //}catch(PDOException $e){
+  //  echo 'Connection Error : ' . $e->getMessage() . '<br>';
+  //}
   var_dump(phpversion());
   //var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
 ?>
