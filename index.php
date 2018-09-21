fff42a88ade92c428509a33c00858b500966e293
TeraokaAkihiro <macrosfronter7@gmail.com>
2018-09-21 10:24:12 +0900

    remove unwanted code.

--- index.php
+++ index.php
@@ -3,12 +3,10 @@
   require_once("./model.php");
 
   try{
-    $pdo = get_db_connect();
+    $dbh = get_db_connect();
   }catch(PDOException $e){
     echo 'Connection Error : ' . $e->getMessage() . '<br>';
   }
-  var_dump(phpversion());
-  var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
 
   include_once('./view.php');
 
